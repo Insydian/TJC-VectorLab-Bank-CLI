f3cb339b18c0cdfb5ad8c282fba7089a6cf40f78
Insydian <cr.larue03@gmail.com>
2023-01-29 23:06:44 -0600

    try3

---
+++ Bank.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "BankAccount.h"
+#include "MyVector.h"
+#include "Person.h"
+using namespace std;
+
+int AddBankAccount(MyVector<BankAccount> bankAccounts);
+int AddPerson(MyVector<Person> people);
+int AddClient(MyVector<BankAccount> bankAccounts, MyVector<Person> people);
+/// Code By Caleb LaRue
+/// Bank is designed as the interface between BankAccounts and people
+int main (int argc, char *argv[])
+{
+    MyVector<BankAccount> bankAccounts;
+    MyVector<Person> clients;
+    return 0;
+}
+int AddBankAccount(MyVector<BankAccount> bankAccounts)
+{
+    return 0;
+}
+
+int AddPerson(MyVector<Person> people)
+{
+    return 0;
+}
+
+int AddClient(MyVector<BankAccount> bankAccounts, MyVector<Person> people)
+{
+    return 0;
+}
