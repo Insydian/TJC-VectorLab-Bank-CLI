44d5af2eb2d8be9dfe54c6203f051ceda226bcbd
Insydian <cr.larue03@gmail.com>
2023-01-31 03:06:37 -0600

    Add logic for addPerson

--- Bank.cpp
+++ Bank.cpp
@@ -5,39 +5,39 @@
 using namespace std;
 
 int addBankAccount(BankAccount account1);
-int addPerson(MyVector<Person> people);
+int addPerson(Person client);
 int addClient(MyVector<BankAccount> bankAccounts, MyVector<Person> people);
-/// Code By Caleb LaRue
-/// Bank is designed as the interface between BankAccounts and people
+// Code By Caleb LaRue
+// Bank is designed as the interface between BankAccounts and people
 MyVector<BankAccount> bankAccounts;
 MyVector<Person> clients;
 int main (int argc, char *argv[])
 {
     BankAccount b1;
     addBankAccount(b1);
-    cout << "back in main" << endl;
-    cout << " testing AccountNumber" << bankAccounts.getElementAt(0).getBankAccountNumber() << endl;
-    for(int i = 0; i < bankAccounts.getSize(); ++i)
-    {
-        cout << bankAccounts.getElementAt(i).getBankAccountNumber() << endl;
-    }
-    
+    Person p1;
+    p1.setName("Caleb LaRue");
+    p1.setAge(18);
+    p1.setCreditScore(700);
+    addPerson(p1);
+    cout << clients.getElementAt(0).getName() << endl;
     return 0;
 }
 int addBankAccount(BankAccount account1)
 {
     const BankAccount constAccount = account1;
-    int capacity = bankAccounts.getCapacity();
-    int size = bankAccounts.getSize();
     
     bankAccounts.pushBack(constAccount);
-    cout << "It worked" <<endl; 
-    return 0;
+    int size = bankAccounts.getSize();
+    return size;
 }
-
-int addPerson(MyVector<Person> people)
+//TODO: the person needs to be established before entering this class. I have a default constructor but I need to add Name etc...
+int addPerson(Person client)
 {
-    return 0;
+    const Person constClient = client;
+    clients.pushBack(constClient);
+    int size = clients.getSize();
+    return size;
 }
 
 int addClient(MyVector<BankAccount> bankAccounts, MyVector<Person> people)
